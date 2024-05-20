eeb7afa04638151aca9b7bffa72e52c9e79f5f9b
Allison <suprun-s@bk.ru>
2024-05-20 23:28:07 +0300

    initial commit

--- Sources/CustomToggle/CustomToggle.swift
+++ Sources/CustomToggle/CustomToggle.swift
@@ -1,2 +1,32 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+public struct MyCustomView<T: CaseIterable & RawRepresentable>: View where T.AllCases: RandomAccessCollection, T.RawValue == String {
+    let item: T.Type
+    @State private var selectedTheme: T
+    let onTap: (T) -> Void
+
+    public init(item: T.Type, selectedTheme: T? = nil, onTap: @escaping (T) -> Void) {
+        self.item = item
+        self._selectedTheme = State(initialValue: selectedTheme ?? item.allCases.first!)
+        self.onTap = onTap
+    }
+    public var body: some View {
+        HStack(spacing: 0) {
+            ForEach(item.allCases, id: \.rawValue) { theme in
+                Text(theme.rawValue)
+                    .padding(.vertical, 10)
+                    .frame(width: 100)
+                    .contentShape(Rectangle())
+                    .background(theme == selectedTheme  ? Color.gray : Color.clear)
+                    .cornerRadius(5)
+                    .onTapGesture {
+                        selectedTheme = theme
+                        onTap(theme)
+                    }
+            }
+        }
+        .padding(3)
+        .background(Color.primary.opacity(0.06), in: Capsule())
+        .padding(.top, 20)
+    }
+}
